772801cc30eba1cf9803eee565e6f74e4df917e9
vinhngo1907 <vinhtrungngo1907@gmail.com>
2023-08-17 19:30:14 +0700

    validate middleware

---
+++ src/middlewares/validate.middleware.js
@@ -0,0 +1,14 @@
+import { APIError } from "../utils/error.util.js";
+const ObjectId = require("mongoose").Types.ObjectId;
+
+const checkReqParam = (req, res, next) => {
+    try {
+        const id = req.params.id;
+        if (id && ObjectId.isValid(id)) next();
+        throw new APIError({ message: 'Id wrong!' });
+    } catch (error) {
+        next(error);
+    }
+}
+
+export default { checkReqParam };
