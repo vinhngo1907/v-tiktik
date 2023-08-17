a97df8f8570596cd09cff80c96a0f508f9d52e96
vinhngo1907 <vinhtrungngo1907@gmail.com>
2023-08-17 15:03:13 +0700

    commit

--- index.js
+++ index.js
@@ -2,14 +2,15 @@ import express from "express";
 import cors from "cors";
 import get from "./src/configs/env.config.js";
 import { connectDB } from "./src/configs/db.config.js";
-import moment from "moment";
 import { createServer } from "http";
 import { Server } from "socket.io";
 import { handleError, handleNotFoundPage } from "./src/middlewares/error.middlware.js";
 import morgan from 'morgan';
-import { join } from 'path';
 import userRoute from "./src/routes/user.route.js";
-import rfs from 'rotating-file-stream';
+import oidc from 'express-openid-connect';
+import * as fs from 'fs';
+const auth = oidc.auth;
+import moment from "moment";
 
 const config = {
     authRequired: false,
@@ -28,18 +29,11 @@ const httpServer = createServer(app);
 const io = new Server(httpServer);
 
 app.use(express.json());
-app.use(express.static("./public"));
 app.use(express.urlencoded({ extended: false }));
 app.use(cors());
-
-// create a rotating write stream
-// const accessLogStream = rfs('access.log', {
-//     interval: '1d', // rotate daily
-//     path: join(__dirname, 'log'),
-// });
-
-// adding morgan to log HTTP requests
-// app.use(isProduction ? morgan('combined', { stream: accessLogStream }) : morgan('dev'));
+app.use(morgan('dev'));
+app.use(auth(config));
+app.use(express.static("./public"));
 
 // connect DB
 connectDB();
@@ -49,7 +43,15 @@ app.get('/', (req, res) => {
     res.send('App works!!!!!');
 });
 
+// Config routes
 app.use('/user', userRoute());
+
+app.get("/login", (req, res) => {
+    res.oidc.login();
+});
+app.get("/logout", (req, res) => {
+    req.oidc.logout();
+});
 
 // request to handle undefined or all other routes
 app.get('*', (req, res) => {
