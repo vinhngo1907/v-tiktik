90f9de57cfbbc3948b8183065f790fc50c46b66f
vinhngo1907 <vinhtrungngo1907@gmail.com>
2023-08-22 19:41:48 +0700

    commit

--- src/services/video.service.js
+++ src/services/video.service.js
@@ -1,6 +1,8 @@
 import VideoModel from '../models/video.model.js';
+import UserModel from '../models/user.model.js';
 import Queue from "../utils/queue.util.js";
 import io from "../../index.js";
+import { getYoutubeVideo } from './youtube.service.js';
 
 const videoQueue = new Queue();
 let seniorSongs = [];
@@ -18,7 +20,7 @@ export async function getVideoById(id) {
     }
 }
 
-export function getAll(){
+export function getAll() {
     try {
         return songsForQueue;
     } catch (error) {
@@ -26,11 +28,22 @@ export function getAll(){
     }
 }
 
-export async function createVideo(youtubeVideoId, authorEmail){
+export async function createVideo(youtubeVideoId, authorEmail) {
     try {
-        
+        const { title, thumbnailUrl, duration } = await getYoutubeVideo(youtubeVideoId);
+        const user = await UserModel.findOne({ email: authorEmail });
     } catch (error) {
         console.log(error.message);
         throw error;
+    }
+}
+
+export async function deleteVideo(id) {
+    try {
+        videoQueue.deleteVideo(id);
+        io.emit("new-video-add", {});
+        return await VideoModel.findByIdAndDelete(id);
+    } catch (error) {
+
     }
 }
